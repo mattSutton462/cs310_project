827daad1e2b6c985d5b6b7b0437bbb9a69f7253d
mattSutton462 <suttonmatthew462@gmail.com>
2023-09-28 10:15:11 -0400

    Fully functional navbar and added about.html

---
+++ script.js
@@ -0,0 +1,42 @@
+(function(){
+    window.addEventListener('load', init);
+
+    function init(){
+        let homeBtn = document.getElementById('logo');
+        homeBtn.addEventListener('click', goHome);
+
+    }
+
+    function goHome(){
+        window.location.href="index.html";
+    }
+
+
+    /* ------------------------------ Helper Functions ------------------------------ */
+  /**
+   * Returns the element that has the ID attribute with the specified value.
+   * @param {string} idName - element ID
+   * @returns {object} DOM object associated with id.
+   */
+  function id(idName) {
+    return document.getElementById(idName);
+  }
+
+  /**
+   * Returns the first element that matches the given CSS selector.
+   * @param {string} query - CSS query selector.
+   * @returns {object[]} array of DOM objects matching the query.
+   */
+  function qs(query) {
+    return document.querySelector(query);
+  }
+
+  /**
+   * Returns the array of elements that match the given CSS selector.
+   * @param {string} query - CSS query selector
+   * @returns {object[]} array of DOM objects matching the query.
+   */
+  function qsa(query) {
+    return document.querySelectorAll(query);
+  }
+})()
