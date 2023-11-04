6ee083791f9d0ea14995511a75bcbf712209a116
HollabaughC <143000835+HollabaughC@users.noreply.github.com>
2023-11-03 22:05:36 -0400

    Added most of the db code
    Need to iron out a bit and add username, should be up and running soon

--- static/script.js
+++ static/script.js
@@ -1,6 +1,19 @@
 (function(){
   "use strict";
 
+  const mysql = require('mysql');
+
+  const db = mysql.createConnection({
+    host: "localhost",
+    user: "wait to insert user, I do not know the name",
+    password: "Woodstock#3"
+  });
+  
+  db.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+
     window.addEventListener('load', init);
 
     function init(){
