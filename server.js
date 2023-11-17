c096ce307653e7a51674bb90767469b20a114660
mattSutton462 <suttonmatthew462@gmail.com>
2023-11-17 09:58:05 -0500

    Update server.js

--- server.js
+++ server.js
@@ -11,10 +11,33 @@
 //     console.log("Connected!");
 //   });
 
+const express = require("express");
+const multer = require("multer");
 const sqlite = require("sqlite");
 const sqlite3 = require("sqlite3");
 
+const app = express();
+const PORT = process.env.PORT || 8080;
+const DB_PATH = "database.db";
+
+app.use(express.urlencoded({ extended: true })); // for application/x-www-form-urlencoded
+app.use(express.json()); // for application/json
+app.use(multer().none()); // for multipart/form-data (required with FormData)
+
 app.use(express.static('static'));
+
+app.get('/events', async function (req, res){
+    const type = req.query.list;
+    try{
+        
+
+
+    } catch (error){
+        console.log(error);
+        res.status(500).send('Error on the server. Please try again later.');
+    }
+
+});
 
 
 function checkStatus(response) {
