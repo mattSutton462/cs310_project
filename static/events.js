4428964d40c8255521d0e0e9814ecc1d63c084be
HollabaughC <143000835+HollabaughC@users.noreply.github.com>
2023-11-22 10:42:50 -0500

    Almost finished events

--- static/events.js
+++ static/events.js
@@ -9,7 +9,19 @@
 
     function fetchEvents(){
         fetch(BASE_URL)
-        .then(checkStatus);
+        .then(checkStatus)
+        .then((data)=>{
+            data = data;
+            fillEvents(data);
+        });
+    }
+
+    function fillEvents(data){
+        for (let i = 0; i < data.length; i++) {
+            let e = document.createElement("p");
+            e.innerHTML = data.location + " " + data.event_date + " " + data.event_time + " " + data.event_type;
+            document.getElementById('event-space').appendChild(e);
+        }
     }
 
     function checkStatus(response) {
